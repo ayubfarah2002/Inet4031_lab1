7a0911852a8ebebbff456b467de4d43ef2f474d0
Ayub Farah <farah357@umn.edu>
2023-02-03 00:28:14 +0000

    first commit

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-  #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-  a = 2
-  b = 2
-  c = a + b
-  print("Python says: Hello, World!")
-  print("%s + %s = %s" % (a,b,c))
+a = 2
+b = 2
+c = a + b
+
+print("Python says: Hello, World!")
+print("%s + %s = %s" % (a,b,c))
